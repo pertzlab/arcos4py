664325856e63658626383c57e8e6a00defc7ab61
Benjamin Graedel <benjamin.graedel@unibe.ch>
2022-12-04 18:09:16 +0100

    changes to imagedata allowing for 3d images and variable median and gaussian filter sizes

--- arcos4py/tools/imagedata.py
+++ arcos4py/tools/imagedata.py
@@ -1,20 +1,21 @@
 """Toolset to convert image data to a dataframe for use with ARCOS."""
 
-import pandas as pd
+from typing import Tuple, Union
+
 import numpy as np
-from typing import Union, Tuple
+import pandas as pd
 from scipy.ndimage import gaussian_filter, median_filter
 from skimage.util import view_as_blocks
+from scipy.ndimage import binary_erosion, binary_fill_holes
 
 
-
-def image_to_dataframe(image: np.ndarray, thresh: int = 0) -> pd.DataFrame:
+def _2d_image_to_dataframe(image: np.ndarray, thresh: int = 0) -> pd.DataFrame:
     """Converts a 2d image series to a dataframe with columns for x, y, and intensity.
     to be used with ARCOS.
 
     Arguments:
-        image (np.ndarray): Image to convert.
-        thresh (int): Threshold to use to remove background.
+        image (np.ndarray): Image to convert. Assumes first axis is t and the two last axes are y and x.
+        thresh (int): Threshold to use to remove background. Default is 0.
 
     Returns (pd.DataFrame): Dataframe with image data.
     """
@@ -24,36 +25,89 @@ def image_to_dataframe(image: np.ndarray, thresh: int = 0) -> pd.DataFrame:
         allArray = np.dstack((indicesArray, i)).reshape((-1, 3))
         df = pd.DataFrame(allArray, columns=["y", "x", "value"])
         df_all.append(df)
-        df['t'] = idx        
-    df_all = pd.concat(df_all)
-    df_all['track_id'] = np.tile(np.arange(len(df_all[df_all.t==0])), len(df_all.t.unique()))
-    df_all = df_all[df_all['value'] > 0]
-    return df_all
+        df['t'] = idx
+    df_all_combined = pd.concat(df_all)
+    df_all_combined['track_id'] = np.tile(
+        np.arange(len(df_all_combined[df_all_combined.t == 0])), len(df_all_combined.t.unique())
+    )
+    df_all_combined = df_all_combined[df_all_combined['value'] > thresh]
+    return df_all_combined
 
-def remove_image_background(image: np.ndarray, filter_type: str = 'gaussian', size: Union[int, Tuple] = 10  ) -> np.ndarray:
-    """Removes background from d2 images. Assumes the two last axes are x and y.
+
+def _3d_image_to_dataframe(image: np.ndarray, thresh: int = 0) -> pd.DataFrame:
+    """Converts a 3d image series to a dataframe with columns for x, y, z, and intensity.
+    to be used with ARCOS.
+
+    Arguments:
+        image (np.ndarray): Image to convert. Assumes first axis is t and the three last axes are z, y, and x.
+        thresh (int): Threshold to use to remove background. Default is 0.
+
+    Returns (pd.DataFrame): Dataframe with image data.
+    """
+    df_all = []
+    for idx, i in enumerate(image):
+        indicesArray = np.moveaxis(np.indices(i.shape), 0, 3)
+        allArray = np.dstack((indicesArray, i)).reshape((-1, 4))
+        df = pd.DataFrame(allArray, columns=["z", "y", "x", "value"])
+        df_all.append(df)
+        df['t'] = idx
+    df_all_combined = pd.concat(df_all)
+    df_all_combined['track_id'] = np.tile(
+        np.arange(len(df_all_combined[df_all_combined.t == 0])), len(df_all_combined.t.unique())
+    )
+    df_all_combined = df_all_combined[df_all_combined['value'] > thresh]
+    return df_all_combined
+
+
+def image_to_dataframe(image: np.ndarray, thresh: int = 0):
+    """Converts an image series to a dataframe with columns for x, y, z, and intensity. Assumes axis order (t, y, x) for 2d images and (t, z, y, x) for 3d images.
+
+
+    Arguments:
+        image (np.ndarray): Image to convert. Assumes first axis is t and the three last axes are z, y, and x.
+        thresh (int): Threshold to use to remove background. Default is 0.
+
+    Returns (pd.DataFrame): Dataframe with image data.
+    """
+    if image.ndim == 3:
+        return _2d_image_to_dataframe(image, thresh)
+    elif image.ndim == 4:
+        return _3d_image_to_dataframe(image, thresh)
+    else:
+        raise ValueError(f'Image must have 3 or 4 dimensions. Image has {image.ndim} dimensions.')
+
+
+def remove_image_background(
+    image: np.ndarray, filter_type: str = 'gaussian', size: Union[int, Tuple] = (10, 1, 1)
+) -> np.ndarray:
+    """Removes background from images. Assumes axis order (t, y, x) for 2d images and (t, z, y, x) for 3d images.
 
     Arguments:
         image (np.ndarray): Image to remove background from.
         filter_type (Union[str, function]): Filter to use to remove background. Can be one of ['median', 'gaussian'].
-        filter_size (int, Tuple): Size of filter to use. For median filter, this is the size of the window. 
-            For gaussian filter, this is the standard deviation. If a tuple is passed then the first value is used for t, the second for x, and the third for y.
+        size (int, Tuple): Size of filter to use. For median filter, this is the size of the window.
+            For gaussian filter, this is the standard deviation. If a single int is passed in, it is assumed to be the same for all dimensions.
+            If a tuple is passed in, it is assumed to correspond to the size of the filter in each dimension.
+            Default is (10, 1, 1).
 
-    Returns (np.ndarray): Image
+    Returns (np.ndarray): Image with background removed.
+        Along the first axis (t) half of the filter size is removed from the beginning and end respectively.
     """
     ## correct images with a gaussian filter applied over time wiht a sigma shift
-    if filter_type not in ['median', 'gaussian'] and not callable(filter_type):
-        raise ValueError('Filter type must be one of ["median", "gaussian"]')
+    allowed_filters = ['median', 'gaussian']
+    if filter_type not in allowed_filters:
+        raise ValueError(f'Filter type must be one of {allowed_filters}.')
     orig_image = image.copy()
     shift = 20
     s_2 = shift // 2
-    if filter_type  in ['median', 'gaussian']:
-        if isinstance(size, int):
-            size = (size, 1, 1)
-        if len(size) == 2:
-            size = (size[0], size[1], size[1])
-        if len(size) == 3:
-            size = (size[0], size[1], size[2])
+
+    if isinstance(size, int):
+        size = (size,) * image.ndim
+    elif isinstance(size, tuple):
+        if len(size) != image.ndim:
+            raise ValueError(f'Filter size must have {image.ndim} dimensions.')
+    else:
+        raise ValueError(f'Filter size must be an int or tuple.')
 
     if filter_type == 'median':
         filtered = median_filter(orig_image, size=size)
@@ -74,19 +128,19 @@ def blockwise_median(a, blockshape):
 
     Returns (np.ndarray): Blockwise median of array.
     """
-    assert a.ndim == len(blockshape), \
-        "blocks must have same dimensionality as the input image"
-    assert not (np.array(a.shape) % blockshape).any(), \
-        "blockshape must divide cleanly into the input image shape"
+    assert a.ndim == len(blockshape), "blocks must have same dimensionality as the input image"
+    assert not (np.array(a.shape) % blockshape).any(), "blockshape must divide cleanly into the input image shape"
 
     block_view = view_as_blocks(a, blockshape)
-    assert block_view.shape[a.ndim:] == blockshape
-    block_axes = [*range(a.ndim, 2*a.ndim)]
+    assert block_view.shape[a.ndim :] == blockshape
+    block_axes = [*range(a.ndim, 2 * a.ndim)]
     return np.median(block_view, axis=block_axes)
 
 
-def mask_image(image: np.ndarray, mask: np.ndarray) -> np.ndarray:
-    """Masks an image with a mask.
+def mask_image(image: np.ndarray, mask: np.ndarray, mask_erosion_iterations: int = 1) -> np.ndarray:
+    """Masks an image with a binary or label image. Can be used to
+    remove edge artifacts that occur when using temporal filtering as
+    implemented in remove_image_background.
 
     Arguments:
         image (np.ndarray): Image to mask.
@@ -94,4 +148,9 @@ def mask_image(image: np.ndarray, mask: np.ndarray) -> np.ndarray:
 
     Returns (np.ndarray): Masked image.
     """
-    return np.where(mask, image, 0)
+    if mask.shape != image.shape:
+        raise ValueError(f'Mask must have same shape as image. Mask shape: {mask.shape}, Image shape: {image.shape}')
+    if mask_erosion_iterations > 0:
+        mask = binary_erosion(mask, iterations=mask_erosion_iterations)
+
+    return np.where(mask > 0, image, 0)
