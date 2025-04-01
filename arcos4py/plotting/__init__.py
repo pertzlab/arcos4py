3344ef05ec7c026cac5add12ec44f44a676f7872
Benjamin <benjamin.graedel@unibe.ch>
2025-04-01 15:37:55 +0200

    added timelapse export for collective events

--- arcos4py/plotting/__init__.py
+++ arcos4py/plotting/__init__.py
@@ -4,6 +4,6 @@ __author__ = """Benjamin Graedel"""
 __email__ = "benjamin.graedel@unibe.ch"
 __version__ = '0.2.5'
 
-from ._plotting import LineagePlot, NoodlePlot, dataPlots, plotOriginalDetrended, statsPlots
+from ._plotting import LineagePlot, NoodlePlot, dataPlots, plotOriginalDetrended, statsPlots, save_animation_frames
 
-__all__ = ["plotOriginalDetrended", "dataPlots", "statsPlots", "NoodlePlot", "LineagePlot"]
+__all__ = ["plotOriginalDetrended", "dataPlots", "statsPlots", "NoodlePlot", "LineagePlot", "save_animation_frames"]
